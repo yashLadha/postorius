f6bf8e5c7209a1c53995631711a97205d42d4e3b
benste <Benedict.Stein+ubuntu@gmail.com>
2011-08-11 10:17:52 +0200

    reenabled tests - including missing file from rev97 - thanks to Flo

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,8 +7,7 @@
 # the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-#
-# GNU Mailman is distributed in the hope that it will be useful, but WITHOUT
+# GNU Mailman is distributed in the hope that it will be useful, but WITHOUT 
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
@@ -16,26 +15,7 @@
 # You should have received a copy of the GNU General Public License along with
 # GNU Mailman.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+import tests
+__test__ = {
+    "Doctest": tests,
+}
