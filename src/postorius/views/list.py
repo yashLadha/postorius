f115a87cfa67dccb634d24bd0bd7e6c36aceadd8
Bhavesh Goyal <bhavesh.goyal093@gmail.com>
2015-04-14 02:04:24 +0530

    Fixed access permissions for mass subscriptions

--- src/postorius/views/list.py
+++ src/postorius/views/list.py
@@ -22,6 +22,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import (login_required,
                                             user_passes_test)
 from django.core.urlresolvers import reverse
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.utils.decorators import method_decorator
@@ -249,22 +251,20 @@ class ListMassSubsribeView(MailingListView):
         else:
             emails = request.POST["emails"].splitlines()
             for email in emails:
-                parts = email.split('@')
-                if len(parts) != 2 or '.' not in parts[1]:
+                try:
+	            validate_email(email)
+                    self.mailing_list.subscribe(address=email)
+                    messages.success(request,
+                                   'The address %s has been subscribed to %s.' %
+                                    (email, self.mailing_list.fqdn_listname))
+                except MailmanApiError:
+                    return utils.render_api_error(request)
+                except HTTPError, e:
+                    messages.error(request, e)
+                except ValidationError:
                     messages.error(request,
                                    'The email address %s is not valid.' %
                                    email)
-                else:
-                    try:
-                        self.mailing_list.subscribe(address=email)
-                        messages.success(
-                            request,
-                            'The address %s has been subscribed to %s.' %
-                            (email, self.mailing_list.fqdn_listname))
-                    except MailmanApiError:
-                        return utils.render_api_error(request)
-                    except HTTPError, e:
-                        messages.error(request, e)
         return redirect('mass_subscribe', self.mailing_list.list_id)
 
 
