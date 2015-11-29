d0141d8c3cad4e31ce41a11d607114368be5843b
Simon Hanna <simon.h4nn4@gmail.com>
2015-11-29 23:24:28 +0100

    Remove unused templates
    also remove templatetags that are not used

--- src/postorius/templatetags/nav_helpers.py
+++ src/postorius/templatetags/nav_helpers.py
@@ -33,25 +33,6 @@ def list_nav(context, current, title=None):
                 title=title)
 
 
-@register.inclusion_tag('postorius/menu/mm_user_nav.html', takes_context=True)
-def user_nav(context, current, title=None):
-    if title is None:
-        title = ''
-    return dict(mm_user=context['mm_user'],
-                current=current,
-                user=context['request'].user,
-                title=title)
-
-
-@register.inclusion_tag('postorius/menu/users_nav.html', takes_context=True)
-def users_nav(context, current, title=None):
-    if title is None:
-        title = ''
-    return dict(current=current,
-                user=context['request'].user,
-                title=title)
-
-
 @register.simple_tag
 def page_url(view_name, *args, **kwargs):
     return reverse(view_name, *args, **kwargs)
