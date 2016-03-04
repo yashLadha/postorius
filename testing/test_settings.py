ba63792cb840f68a0bab2bfd979a04912d70ebe1
Simon Hanna <user.name>
2016-03-04 15:18:16 +0100

    Update year to 2016

--- testing/test_settings.py
+++ testing/test_settings.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-# Copyright (C) 1998-2012 by the Free Software Foundation, Inc.
+# Copyright (C) 1998-2016 by the Free Software Foundation, Inc.
 #
 # This file is part of Postorius.
 #
