3bf62077a6e1273697d9beed503badc45fc10817
Aditi <aditi.medha96@gmail.com>
2016-06-07 19:39:03 +0530

    Add filter for essay

--- src/postorius/views/essay.py
+++ src/postorius/views/essay.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from postorius.models import EssaySubscribe
 
 def EssayList(request,list_id,email):
-	list_essay = EssaySubscribe.objects.filter(list_id=list_id, email=email)
-	return render(request , 'postorius/lists/essay_list.html', {'object':list_essay})
-	
+    list_essay = EssaySubscribe.objects.filter(list_id=list_id, email=email).order_by('-date')
+    return render(request , 'postorius/lists/application.html', {'object':list_essay})
+    
