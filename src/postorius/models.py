69702c31864f23a9add159fd012bd707e3281a69
Aditi <aditi.medha96@gmail.com>
2016-08-16 18:01:07 +0530

    Add method in Unsubscriberstats class to compare

--- src/postorius/models.py
+++ src/postorius/models.py
@@ -328,11 +328,19 @@ class UnsubscriberStats(models.Model):
     email = models.EmailField()
     channel = models.CharField(max_length=100)
     date = models.DateTimeField(default=datetime.now())
+    user_id = models.IntegerField()
+    user = models.CharField(max_length=100)
 
     def __str__(self):
-        return u'%s   %s   %s' % (self.list_id, self.email, self.channel)
+        return u'%s   %s   %s   %s  %d %s' % (self.list_id, self.email, self.channel,self.user,self.user_id,self.date)
 
     @classmethod
-    def create(cls,list_id,email,channel,date):
-        return cls(list_id=list_id,email=email,channel=channel,date=date)
+    def create(cls,list_id,email,channel,date,user_id,user):
+        return cls(list_id=list_id,email=email,channel=channel,date=date,user_id=user_id,user=user)
+
+    def __hash__(self):
+        return hash(self.user_id)
+
+    def __eq__(self, other):
+        return self.user_id == other.user_id
    
